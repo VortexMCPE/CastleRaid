54df4425ed1a85a4a1298890e9887f6536c7cf86
QuiverlyRivalry <dungeonwolfmc@gmail.com>
2017-09-28 11:43:40 +0100

    Update spawn.php

--- src/AlwaysSpawn/spawn.php
+++ src/AlwaysSpawn/spawn.php
@@ -1,3 +1,17 @@
 <?php
 
 namespace AlwaysSpawn;
+
+use pocketmine\plugin\PluginBase;
+
+class AlwaysSpawn extends PluginBase{
+
+          public function onLoad(){
+                    $this->getLogger()->info("Plugin Loading");
+          }
+          public function onEnable(){
+                    $this->getLogger()->info("Enabled Plugin");
+          }
+          public function onDisable(){
+                    $this->getLogger()->info("Plugin Disabled");
+          }
