443262fe0621a036bfa8851c6aaa40937cfa3b95
QuiverlyRivalry <dungeonwolfmc@gmail.com>
2017-09-28 20:58:47 +0100

    Update spawn.php

--- src/AlwaysSpawn/spawn.php
+++ src/AlwaysSpawn/spawn.php
@@ -1,29 +1,29 @@
+
 <?php
 
 namespace AlwaysSpawn;
-
-use pocketmine\event\player\PlayerRespawnEvent;
-
-use pocketmine\Player;
-
-use pocketmine\Server;
-
+use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerLoginEvent;
 
-class AlwaysSpawn extends PluginBase implements Listener{
-
+class Loader extends Plugin implements Listener{
+  
+  
           public function onLoad(){
                     $this->getLogger()->info("Plugin Loading");
           }
           public function onEnable(){
-                    $this->getServer()->getPluginManager()->registerEvents($this,$this);
-		    $this->getLogger()->info("Enabled Plugin");
+                    $this->getLogger()->info("Enabled Plugin");
           }
           public function onDisable(){
                     $this->getLogger()->info("Plugin Disabled");
-	  }
-	  public function onPlayerRespawn(PlayerRespawnEvent $event){
-    		    $player->setLevel($level);
-    	            $level = $this->getServer()->getDefaultLevel();
-	}
-}
+          }
+  
+  public function onPlayerLogin(PlayerLoginEvent $event){
+    $player = $event->getPlayer();
+    $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
+    $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
+    $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
+    $player->setLevel($level);
+    }
+  }
