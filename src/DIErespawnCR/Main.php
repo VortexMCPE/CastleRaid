bdf42d9f6a4f3c25f5bfa46044626a6eadc9d6cd
QuiverlyRivalry <dungeonwolfmc@gmail.com>
2017-10-07 01:41:36 +0100

    Update Main.php

--- src/DIErespawnCR/Main.php
+++ src/DIErespawnCR/Main.php
@@ -7,22 +7,18 @@ use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\math\Vector3;
 
 class Main extends PluginBase implements Listener{
-  
-  public function onEnable(){
-    $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $this->getServer()->getLogger()->info("DIErespawnCR Enabled!");
+ 
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getLogger()->info("DIErespawnCR Enabled!");
     }
-  
-  public function onDisable(){
-    $this->getServer()->getLogger()->info("DIErespawnCR Disabled!");
+ 
+    public function onDisable(){
+        $this->getServer()->getLogger()->info("DIErespawnCR Disabled!");
     }
-  
-  public function onPlayerRespawn(PlayerRespawnEvent $event){
-    $player = $event->getPlayer();
-    $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
-    $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
-    $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
-    $player->setLevel($level);
-    $player->teleport(new Vector3($x, $y, $z));
+ 
+    public function onPlayerRespawn(PlayerRespawnEvent $event){
+        $player = $event->getPlayer();
+        $player->teleport($this->level->getSpawn());
     }
-  }
+}
