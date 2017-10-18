700336fa6da837d0a7ffdee55fd3354f6631e566
QuiverlyRivalry <dungeonwolfmc@gmail.com>
2017-10-18 10:11:10 +0100

    Update Task.php

--- src/CRcore/Task.php
+++ src/CRcore/Task.php
@@ -1,3 +1,24 @@
 <?php
 
+namespace CRcore;
+
+use pocketmine\scheduler\PluginTask;
+use Narwhal\Main;
+
+class Task extends PluginTask{
+
+public $playername;
+
+	public function __construct(Main $main, string $playername){
+		parent::__construct($main);
+		$this->playername = $playername;
+	}
+	
+	public function onRun(int $tick){
+	$player = $this->getOwner()->getServer()->getPlayer($this->playername());
+		if($player instanceof Player){
+			$player->addTitle("§aCastle§l§2Raid, "§7Welcome!", -1, 5*20, 30);
+		}
+	}
+
 
