29cc95673d904003ce3186cf41aa92f3c1cc8771
YaBoiLewis <lewis@creeperlewis.com>
2017-10-28 19:07:58 +0500

    Update Main.php

--- src/CRcore/Main.php
+++ src/CRcore/Main.php
@@ -8,7 +8,7 @@ use pocketmine\math\Vector3;
 use pocketmine\scheduler\Scheduler;
 
 use CRcore\auth\Auth;
-use CRcore\listener\EventListener;
+use CRcore\EventListener;
 
 class Main extends PluginBase {
     
@@ -19,15 +19,5 @@ class Main extends PluginBase {
     public function onDisable() {
         $this->getServer()->getLogger()->info("DIErespawnCR Disabled!");
     }
-    public function onRespawn(PlayerRespawnEvent $event) {
-        $player = $event->getPlayer();
-        $player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-    }
-    public function onJoin(PlayerJoinEvent $e) {
-        $player = $e->getPlayer();
-        if ($player->isAuthenicated) {
-            $player->addTitle("§aCastle§l§2Raid, "§7Welcome!", 30, 5*20, 30);
-            $this->getServer()->getLogger()->notice("Told you. Narwhals always win.");
-        }
-    }
+    
 }
