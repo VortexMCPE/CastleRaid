b45d6b073c86e2dee184b467b46fe29846ea0b4c
jasonwynn10 <jasonwynn10@gmail.com>
2017-11-07 19:42:42 -0500

    Fixed all formatting :D Added PhpDoc comments

--- src/CRcore/EventListener.php
+++ src/CRcore/EventListener.php
@@ -1,33 +1,29 @@
 <?php
-
+declare(strict_types=1);
 namespace CRcore;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerRespawnEvent;
 use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\Server;
-use pocketmine\math\Vector3;
-
-use CRcore\Main;
+use pocketmine\event\player\PlayerRespawnEvent;
 
 class EventListener implements Listener {
-		
-		private $plugin;
-		
-		public function __construct(Main $plugin) {
-        $this->plugin = $plugin;
-    }
-		
-		public function onRespawn(PlayerRespawnEvent $event) {
-				$player = $event->getPlayer();
-        $player->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
+	/** @var \CRcore\Main $plugin */
+	private $plugin;
+
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+	}
+
+	public function onRespawn(PlayerRespawnEvent $event){
+		$player = $event->getPlayer();
+		$player->teleport($this->plugin->getServer()->getDefaultLevel()->getSafeSpawn());
+	}
+
+	public function onJoin(PlayerJoinEvent $event){
+		$player = $event->getPlayer();
+		if($player->isAuthenticated()){
+			$player->addTitle("§aCastle§l§2Raid", "§7Welcome!", 30, 5 * 20, 30);
+			$this->plugin->getServer()->getLogger()->notice("Told you. Narwhals always win.");
 		}
-		
-		public function onJoin(PlayerJoinEvent $event) {
-        $player = $event->getPlayer();
-        if ($player->isAuthenicated) {
-            $player->addTitle("§aCastle§l§2Raid, "§7Welcome!", 30, 5*20, 30);
-            $this->getServer()->getLogger()->notice("Told you. Narwhals always win.");
-        }
-    }
+	}
 }
